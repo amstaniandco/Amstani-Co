f594a49e12870921aa2cf088d91541145d2d5df4
Israr Ahmad <asrarking831.aa@gmail.com>
2026-04-11 23:18:12 +0500

    fixed empty spacing

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -6,7 +6,7 @@ export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-full bg-slate-50 text-slate-900">
       <Header />
-      <main className="mx-auto p-8 max-w-screen-xl">{children}</main>
+      <main className="mx-auto p-8 max-w-screen-xxl">{children}</main>
       <Footer />
     </div>
   );
