597bb93d22b5153f56c52d4733856145da7edba6
Israr Ahmad <asrarking831.aa@gmail.com>
2026-04-19 15:14:38 +0500

    fixed home and landing page swapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Header from "../components/pages/Header";
 import Footer from "../components/pages/Footer";
-import CustomerHomePage from "./(customers)/home/page";
+import LandingPage from "./landing/page";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
       <Header />
       <main className="w-full px-4 sm:px-6 lg:px-8 py-6">
-        <CustomerHomePage />
+        <LandingPage />
       </main>
       <Footer />
     </div>
